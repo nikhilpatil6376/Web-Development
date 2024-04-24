9869c56975951b4bf309ae94548543c75b2fda11
nikhilpatil6376 <nikhilpatil6376@gmail.com>
2024-04-24 20:34:40 +0530

    HTML, CSS, JavaScript, React

---
+++ Project-30(Like Button)/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import LikeButton from './LikeButton'
+
+function App() {
+  return (
+    <> 
+      <p>State in React</p>
+      <LikeButton />
+    </>
+  )
+}
+
+export default App
